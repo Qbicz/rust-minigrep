74faa54ae6aa079585fd180a66a20232a960a3c6
Filip Kubicz <filip.kubicz@tier.app>
2023-08-06 15:24:15 +0200

    test: Add unit tests for Config build from CLI

--- src/lib.rs
+++ src/lib.rs
@@ -15,15 +15,61 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let debug = args.len() > 3 && args[3] == "debug";
+        println!("Debug logs: {}", debug);
 
-        Ok(Config {query, file_path})
+        Ok(Config {query, file_path, debug})
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
+    if config.debug {
+        println!("With text:\n{contents}");
+    }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests { // module for unit tests
+    use super::*; // import symbols from parent module
+
+    #[test]
+    fn config_build() {
+        let args = vec![
+            String::from("minigrep"),
+            String::from("query"),
+            String::from("file_path"),
+        ];
+        let config = Config::build(&args).unwrap();
+        assert_eq!(config.query, "query");
+        assert_eq!(config.file_path, "file_path");
+        assert_eq!(config.debug, false);
+    }
+
+    #[test]
+    fn config_build_with_debug() {
+        let args = vec![
+            String::from("minigrep"),
+            String::from("query"),
+            String::from("file_path"),
+            String::from("debug"),
+        ];
+        let config = Config::build(&args).unwrap();
+        assert_eq!(config.query, "query");
+        assert_eq!(config.file_path, "file_path");
+        assert_eq!(config.debug, true);
+    }
+
+    #[test]
+    fn config_build_with_too_few_args() {
+        let args = vec![
+            String::from("minigrep"),
+            String::from("query"),
+        ];
+        let config = Config::build(&args);
+        assert!(config.is_err());
+    }
 }
